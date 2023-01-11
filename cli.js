564e034c952eda162c33d65201a97cc6918993e0
camila leal <khammylv@gmail.com>
2023-01-11 14:54:03 -0500

    proyecto final

--- cli.js
+++ cli.js
@@ -1,39 +1,54 @@
 #!/usr/bin/env node
 const { mdLinks } = require('./index')
+const { statsLinks, statsAndValidateLinks } = require('./module/statValidate')
 
-const [, , ...args] = process.argv;
+const filePath = process.argv[2]
+const argv = process.argv
 
+const cli = (filePath, argv) => {
+    const validate = argv.includes("--validate");
+    const stats = argv.includes("--stats");
+    if (filePath === undefined) {
+        console.log("😱 Please enter a path")
+    }
+    if (stats && validate) {
+        mdLinks(filePath, { validate: true })
+            .then((results) => {
+                const statVal = statsAndValidateLinks(results)
+                console.log(`Total: ${statVal.Total} \nUnique: ${statVal.Unique} \nBroken: ${statVal.Broken}`)
+            })
+            .catch((err) => { console.log(err) })
+    }
+    if (argv.length <= 3) {
+        mdLinks(filePath, { validate: false }).then((results) => {
+            results.forEach(element => {
+                console.log(`${element.file} ${element.href} ${element.text}`)
+            });
+        })
+            .catch((err) => { console.log(err) })
+    }
+    if (validate) {
+        mdLinks(filePath, { validate: true })
+            .then((res) => {
+                res.forEach(element => {
+                    console.log(`${element.file} ${element.href} ${element.message} ${element.status} ${element.text}`)
+                });
+            })
+            .catch((err) => { console.log(err) })
 
+    }
+    if (stats) {
+        mdLinks(filePath, { validate: true })
+            .then((results) => {
 
-const options = {
-    validate: args.includes("--validate"),
-    stats : args.includes("--stats"),
+                const res = statsLinks(results)
+                console.log(`Total: ${res.Total}\nUnique: ${res.Unique}`)
+            })
+            .catch((err) => { console.log(err) })
+    }
+   if(argv !== '--stats' && argv !== '--validate' && argv !== undefined){
+    console.log('😔 Your option is not valid, try using --validate, --stats o --stats --validate')
+   }
 }
 
-mdLinks(args[0], options )
-.then((res) => {
-    
-    if(!options.validate && !options.stats){
-        res.forEach(element => {
-           console.log( `${element.file} ${element.href} ${element.text}`)
-        });
-        return
-       }
-    if (options.validate && options.stats) {
-        console.log(`Total: ${res.Total} \nUnique: ${res.Unique} \nBroken: ${res.Broken}`)
-        return
-     }
-    if(options.validate){
-     res.forEach(element => {
-        console.log( `${element.file} ${element.href} ${element.message} ${element.status} ${element.text}`)
-     });
-     return
-    }
-    if(options.stats){
-        console.log(`Total: ${res.Total}\nUnique: ${res.Unique}`)
-        return
-    }
-   
-})
-.catch((err) => {console.error(err)});
-
+cli(filePath, argv)
--- index.js
+++ index.js
@@ -1,65 +1,25 @@
 const { extractLinks } = require('./module/linksMD')
 const { getStatus } = require('./module/linkStatus')
-const {statsLinks, statsAndValidateLinks }  = require('./module/statValidate')
+
 
 const mdLinks = (route, options) =>{
     return new Promise((resolve, reject) => {
-     
-    const regexVacia = /\s/g
-    if(regexVacia.test(route)){
-     reject('Please enter a path')
-    }  
-    
     extractLinks(route).then((res) => {
-           const respuesta = res.flat()
-           if(options === undefined) {
-            resolve(respuesta)
-           
-           }  
-           if(options.validate && options.stats){
-            getStatus(respuesta)
-            .then((res) => {
-               resolve(statsAndValidateLinks(res))
-            })
-            .catch((err) => {
-                reject(err)
-            });
-            return
-           }
-           if(options.validate){
-            
-             getStatus(respuesta)
-             .then((res) => {
-              resolve(res)
-             })
-             .catch((err) => {
-                reject(err)
-             });
-             return
-           } 
-           if(options.stats){
-               
-                getStatus(respuesta)
-                .then((res) => {
-                   resolve(statsLinks(res))
-                })
-                .catch((err) => {
-                    reject(err)
-                });
-             return   
-           } 
-           else {
-            resolve(respuesta)
-            return
-           
-           }
-        
-        }) 
-        .catch((err) => {reject(err)}); 
+        const respuesta = res.flat() 
+       if(options.validate === true) {
+        getStatus(respuesta).then((status) => {
+            resolve(status)
+        })
+       }
+       if(options.validate === false){
+        resolve(respuesta)
+       }
+       
+    }).catch((err) => {reject(err)});
+    
     })
    
 }
 module.exports = {
     mdLinks
 }
-
