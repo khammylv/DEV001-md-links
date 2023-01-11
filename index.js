6c6c8f950ba08fdf444048e91f10d812963092c8
camila leal <khammylv@gmail.com>
2023-01-11 15:13:03 -0500

    prueba con otro proyecto

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const { extractLinks } = require('./module/linksMD')
 const { getStatus } = require('./module/linkStatus')
 
 
-const mdLinks = (route, options) =>{
+const mdLinks = (route, options) => {
     return new Promise((resolve, reject) => {
-    extractLinks(route).then((res) => {
-        const respuesta = res.flat() 
-       if(options.validate === true) {
-        getStatus(respuesta).then((status) => {
-            resolve(status)
-        })
-       }
-       if(options.validate === false){
-        resolve(respuesta)
-       }
-       
-    }).catch((err) => {reject(err)});
-    
+        extractLinks(route).then((res) => {
+            const respuesta = res.flat()
+            if (options === undefined) {
+                resolve(respuesta)
+            }
+            if (options.validate === true) {
+                getStatus(respuesta).then((status) => {
+                    resolve(status)
+                })
+            }
+            if (options.validate === false) {
+                resolve(respuesta)
+            }
+
+        }).catch((err) => { reject(err) });
+
     })
-   
+
 }
 module.exports = {
     mdLinks
